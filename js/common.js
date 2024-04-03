362f0df9b6630f41617ce38c37e64cb3b5dbb7fa
59Youn <wltjs9659@gmail.com>
2024-04-03 15:27:34 +0900

    반응형 완료

--- js/common.js
+++ js/common.js
@@ -1,6 +1,29 @@
+const ham = document.querySelector('.ham');
+const menu = document.querySelector('.menu');
+const person = document.querySelector('.person');
+
+ham.addEventListener('click', (e) => {
+  e.preventDefault();
+  ham.classList.toggle('on');
+  menu.classList.toggle('on');
+  person.classList.toggle('on');
+});
+
 var swiper = new Swiper('.reviewSlide', {
-  slidesPerView: 3,
+  slidesPerView: 1,
   spaceBetween: 30,
+
+  breakpoints: {
+    860: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1100: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+
   pagination: {
     el: '.pg1',
     clickable: true,
