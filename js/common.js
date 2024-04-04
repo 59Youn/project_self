15f5c8ed87c764b93f41e43be60cdb9f46dd34a7
59Youn <wltjs9659@gmail.com>
2024-04-04 11:11:48 +0900

    검토_2차

--- js/common.js
+++ js/common.js
@@ -1,3 +1,4 @@
+// 햄버거 버튼
 const ham = document.querySelector('.ham');
 const menu = document.querySelector('.menu');
 const person = document.querySelector('.person');
@@ -9,6 +10,35 @@ ham.addEventListener('click', (e) => {
   person.classList.toggle('on');
 });
 
+// section.courses 영역
+const popular = document.querySelector('.popular');
+const recommended = document.querySelector('.recommended');
+const price = document.querySelector('.price');
+const pop = document.querySelector('.pop');
+const rec = document.querySelector('.rec');
+const pri = document.querySelector('.pri');
+const list = document.querySelectorAll('.list');
+
+popular.addEventListener('click', () => {
+  list.forEach((el) => {
+    el.classList.remove('on');
+  });
+  pop.classList.add('on');
+});
+recommended.addEventListener('click', () => {
+  list.forEach((el) => {
+    el.classList.remove('on');
+  });
+  rec.classList.add('on');
+});
+price.addEventListener('click', () => {
+  list.forEach((el) => {
+    el.classList.remove('on');
+  });
+  pri.classList.add('on');
+});
+
+// section.review 영역
 var swiper = new Swiper('.reviewSlide', {
   slidesPerView: 1,
   spaceBetween: 30,
